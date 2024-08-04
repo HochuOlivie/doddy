2bb5c4f4cb05c5980886030421d9ee5f78657b84
kit <emeralddog@list.ru>
2024-08-04 22:32:38 +0500

    requirements

--- doddy/admin.py
+++ doddy/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(DoddyUser)
+admin.site.register(Auction)
+admin.site.register(AuctionBet)
+admin.site.register(FarmType)
+admin.site.register(UserFarm)
